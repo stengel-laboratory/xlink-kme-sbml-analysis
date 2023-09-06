b7c2c10f35af43c263e3ee9b995c0443da4d1380
Kai-Michael Kammer <kai-michael.kammer@uni-konstanz.de>
2023-09-06 16:51:43 +0200

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import versioneer
+import setuptools
+
+
+setuptools.setup(name='xlink_kme_sbml_analysis,
+      description='Analysis of models obtained with the Kinetic Simulation Framework for Crosslinks',
+      author='Kai-Michael Kammer',
+      author_email='kai-michael.kammer@uni-konstanz.de',
+      url='https://github.com/stengel-laboratory/xlink-kme-sbml-analysis',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      packages=setuptools.find_packages(),
+      install_requires=[
+          'numpy',
+          'pandas',
+          'altair',
+          'vegafusion',
+          'vegafusion-python-embed',
+          'vl-convert-python',
+      ],
+      license='MIT',
+      python_requires='>=3.10'
+      )
