7a2dfeba20ec758db6478c3a3185e398e9097ef2
Kai Kammer <29574032+kaisengit@users.noreply.github.com>
2026-08-03 20:18:33 +0200

    fix(packaging): restore Versioneer integration
    Vendor Versioneer and its generated package version module so editable installs derive their version from the repository Git tags.

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-import versioneer
 import setuptools
+import versioneer
 
 
-setuptools.setup(name='xlink_kme_sbml_analysis,
-      description='Analysis of models obtained with the Kinetic Simulation Framework for Crosslinks',
-      author='Kai-Michael Kammer',
-      author_email='kai-michael.kammer@uni-konstanz.de',
-      url='https://github.com/stengel-laboratory/xlink-kme-sbml-analysis',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      packages=setuptools.find_packages(),
-      install_requires=[
-          'numpy',
-          'pandas',
-          'tellurium',
-          'altair[all]',
-      ],
-      license='MIT',
-      python_requires='>=3.10'
-      )
+setuptools.setup(
+    name="xlink_kme_sbml_analysis",
+    description="Analysis of models obtained with the Kinetic Simulation Framework for Crosslinks",
+    author="Kai-Michael Kammer",
+    author_email="kai-michael.kammer@uni-konstanz.de",
+    url="https://github.com/stengel-laboratory/xlink-kme-sbml-analysis",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "tellurium",
+        "altair[all]",
+    ],
+    license="MIT",
+    python_requires=">=3.10",
+)
